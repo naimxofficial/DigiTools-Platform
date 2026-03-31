99b5b900ba5d50bcb5972006f6825f20a2977d2b
Naim Uddin <naimxofficial.bd@gmail.com>
2026-03-31 15:44:02 +0600

    Designed Stats section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Hero from './Components/Hero/Hero'
 import Navbar from './Components/NavBar/Navbar'
+import Stats from './Components/Stats/Stats'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
     <Navbar></Navbar>
     <Hero></Hero>
+    <Stats></Stats>
     </>
   )
 }
---
+++ src/Components/Stats/Stats.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Stats = () => {
+    return (
+        <section className=' bg-linear-to-r from-[#4f39f6] to-[#9514fa] flex items-center py-10 justify-center gap-10'>
+            <div className='text-white space-y-3 border-r border-white/50 text-center md:px-20 px-5'>
+                <h1 className='font-extrabold md:text-5xl text-2xl'>50K+</h1>
+                <p className='font-medium md:text-2xl text-xs'>Active Users</p>
+            </div>
+            <div className='text-white space-y-3 border-r border-white/50 text-center md:px-20 px-5'>
+                <h1 className='font-extrabold md:text-5xl text-2xl'>200+</h1>
+                <p className='font-medium md:text-2xl text-xs'>Premium Tools</p>
+            </div>
+            <div className='text-white space-y-3  text-center md:px-20 px-5'>
+                <h1 className='font-extrabold md:text-5xl text-2xl'>4.9</h1>
+                <p className='font-medium md:text-2xl text-xs'>Rating</p>
+            </div>
+            
+        </section>
+    );
+};
+
+export default Stats;
